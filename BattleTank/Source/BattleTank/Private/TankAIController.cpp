51f0ce7a2ec3a908dc428290db7a09d5800b5b0f
tjaranis <tjaranis@gmail.com>
2018-10-28 22:55:51 +0100

    barrel aim

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,11 +1,19 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankAIController.h"
-
+#include "Engine/World.h"
 
 ATank* ATankAIController::GetControllerTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+// Called every frame
+void ATankAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+	UE_LOG(LogTemp, Warning, TEXT("AIController not possessing tank"));
 }
 
 void ATankAIController::BeginPlay()
@@ -13,7 +21,7 @@ void ATankAIController::BeginPlay()
 	Super::BeginPlay();
 	UE_LOG(LogTemp, Warning, TEXT("test"));
 	auto ControlledTank = GetControllerTank();
-
+	auto PlayerTank = GetPlayerTank();
 
 	if (!ControlledTank)
 	{
@@ -23,4 +31,32 @@ void ATankAIController::BeginPlay()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("AIController possessing: %s"), *(ControlledTank->GetName()));
 	}
+
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerTank not found"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerTank found: %s"), *(PlayerTank->GetName()));
+	}
 }
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerTank) { return nullptr; }
+	return Cast<ATank>(PlayerTank);
+}
+
+void ATankAIController::AimTowardsCrosshair()
+{
+	if (!GetControllerTank()) { return; }
+
+	FVector HitLocation;//out parameter
+	UE_LOG(LogTemp, Warning, TEXT("Hitlocation: %s"), *HitLocation.ToString()) ;
+
+}
+
+
+
